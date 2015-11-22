153a6b56d64b9537e39bb1ac5e6c7a29466b1d5d
Blake Shaw <blake.shaw@gmail.com>
2015-11-22 15:43:57 -0800

    Cleaning up date formatter logic.  Adding a .gitignore.

--- ddate/DateFormatter.cs
+++ ddate/DateFormatter.cs
@@ -1,142 +1,53 @@
-﻿namespace ddate {
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
-class DateFormatter
+﻿namespace ddate
 {
-	private readonly DiscordianDate _ddate;
-	public DateFormatter(DiscordianDate ddate)
-	{
-		_ddate = ddate;
-	}
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using static NumToEng;
 
-	public string GetStandardDate()
+	class DateFormatter
 	{
-		var fullDayName = _ddate.IsStTibsDay
-			? DiscordianDate.StTibsDay.Name
-			: String.Format("{0}, the {2} day of {1}", _ddate.DayName, _ddate.SeasonName, ToRankedNumeric(_ddate.DayOfSeason));
-
-		Console.WriteLine("");
-		const string dateFormat = "Today is {0}, in the Year of Our Lady of Discord {1}";
-		return String.Format(dateFormat, fullDayName, ToEnglishWords(_ddate.Year));
-	}
-
-	public string GetStandardHolyDays()
-	{
-		return "It is " + EnglishJoin(_ddate.TodaysHolyDays);
-	}
-
-	private string EnglishJoin(IEnumerable<string> words)
-	{
-		if (words == null)
-			return String.Empty;
-		if (words == null || !words.Any())
-				return null;
-		switch (words.Count())
+		private readonly DiscordianDate _ddate;
+		public DateFormatter(DiscordianDate ddate)
 		{
-			case 1:
-				return words.First();
-			case 2:
-				return String.Join(" and ", words);
-			default:
-				return String.Join(", ", words.Where(x => x != words.Last())) + ", and " + words.Last();
+			_ddate = ddate;
 		}
-	}
 
-	private static readonly string[] Ones = { null, "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
-	private static readonly string[] Teens = { "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
-	private static readonly string[] Tens = {null, null, "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
-	private static readonly string[] BigNumbers = {"Thousand", "Million", "Billion", "Trillion", "Quadrillion", "Quintillion" };
-
-	public static string ToEnglishWords(long number)
-	{
-		var words = new List<string>();
-		bool isNegative = number < 0;
-
-		GetValue(number, words);
-		for (int ii = 3; ii <= BigNumbers.Length*3; ii += 3)
+		public string GetStandardDate()
 		{
-			long pow = PowTen(ii);
-			if (number/pow > 0)
+			var fullDayName = _ddate.IsStTibsDay
+				? DiscordianDate.StTibsDay.Name
+				: String.Format("{0}, the {2} day of {1}", _ddate.DayName, _ddate.SeasonName, ToRankedNumeric(_ddate.DayOfSeason));
+
+			Console.WriteLine("");
+			const string dateFormat = "Today is {0}, in the Year of Our Lady of Discord {1}";
+			return String.Format(dateFormat, fullDayName, ToEnglishWords(_ddate.Year));
+		}
+
+		public string GetStandardHolyDays()
+		{
+			return "It is " + EnglishJoin(_ddate.TodaysHolyDays);
+		}
+
+		private string EnglishJoin(IEnumerable<string> words)
+		{
+			if (words == null)
+				return String.Empty;
+			var actualWords = words.Where(x => !String.IsNullOrWhiteSpace(x)).ToList();
+
+			switch (actualWords.Count)
 			{
-				var bigText = BigNumbers[ii/3 - 1];
-				words.Add(bigText);
-				if (!GetValue(number/pow, words))
-					words.Remove(bigText);
+				case 0:
+					return String.Empty;
+				case 1:
+					return actualWords.First();
+				case 2:
+					return String.Join(" and ", actualWords);
+				default:
+					var lastWord = actualWords.Last();
+					actualWords.RemoveAt(actualWords.Count - 1);
+					return String.Join(", ", actualWords) + ", and " + lastWord;
 			}
 		}
-
-		words.RemoveAll(x => x == null);
-		words.Reverse();
-		return (isNegative ? "negative " : null) + String.Join("-", words);
 	}
-
-	private static long PowTen(long x)
-	{
-		long y = 10;
-		for (int ii = 1; ii < x; ii++)
-			y *= 10; 
-		return y;
-	}
-
-	private static bool GetValue(long number, List<string> words)
-	{
-		var wordCount = words.Count(x => x != null);
-		if (number == 0) words.Add("zero");
-		number = Math.Abs(number);
-		var ones = number%10;
-		number /= 10; //tens
-		if (number > 0)
-		{
-			var tens = number%10;
-			if (tens == 1)
-			{
-				words.Add(Teens[ones]);
-			}
-			else
-			{
-				words.Add(Ones[ones]);
-				words.Add(Tens[tens]);
-			}
-		}
-		else
-		{
-			words.Add(Ones[ones]);
-		}
-		number /= 10; //hundreds
-		if (number > 0)
-		{
-			if (number%10 != 0)
-			{
-				words.Add("hundred");
-				words.Add(Ones[number%10]);
-			}
-		}
-		return wordCount != words.Count(x => x != null);
-	}
-
-	private string ToRankedNumeric(int number)
-	{
-		return number + GetRankSuffix(number);
-	}
-
-	private string GetRankSuffix(int number)
-	{
-		if ((Math.Abs(number) / 10) % 10 == 1)
-			return "th";
-
-		switch (Math.Abs(number) % 10)
-		{
-			case 1:
-				return "st";
-			case 2:
-				return "nd";
-			case 3:
-				return "rd";
-			default:
-				return "th";
-		}
-	}
-}
 }
---
+++ ddate/NumToEng.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ddate
+{
+	class NumToEng
+	{
+		private static readonly string[] Ones = { null, "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
+		private static readonly string[] Teens = { "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+		private static readonly string[] Tens = {null, null, "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+		private static readonly string[] BigNumbers = {"Thousand", "Million", "Billion", "Trillion", "Quadrillion", "Quintillion" };
+
+		public static string ToEnglishWords(long number, bool useDash = true)
+		{
+			if (number == 0) return "Zero";
+			var words = new List<string>();
+			bool isNegative = number < 0;
+
+			GetOnesToHundreds(number, words);
+			for (int ii = 1; ii <= BigNumbers.Length; ii++)
+			{
+				long pow = number / PowTen(ii*3);
+				if (pow < 1) break;
+
+				words.Add(BigNumbers[ii - 1]);
+				GetOnesToHundreds(pow, words);
+			}
+
+			words.RemoveAll(x => x == null);
+			words.Reverse();
+			return (isNegative ? "Negative " : null) + String.Join(useDash ? "-" : " ", words);
+		}
+
+		public static string ToRankedNumeric(int number)
+		{
+			return number + GetRankSuffix(number);
+		}
+
+		private static long PowTen(long x)
+		{
+			long y = 10;
+			for (int ii = 1; ii < x; ii++)
+				y *= 10; 
+			return y;
+		}
+
+		private static void GetOnesToHundreds(long number, List<string> words)
+		{
+			if (number == 0) return;
+			number = Math.Abs(number);
+			var ones = number%10;
+			var tens = (number/10)%10;
+			var hundreds = (number/100)%10;
+
+			if (tens == 1)
+			{
+				words.Add(Teens[ones]);
+			}
+			else
+			{
+				words.Add(Ones[ones]);
+				words.Add(Tens[tens]);
+			}
+			if (hundreds != 0)
+			{
+				words.Add("Hundred");
+				words.Add(Ones[hundreds]);
+			}
+		}
+
+		private static string GetRankSuffix(int number)
+		{
+			if ((Math.Abs(number) / 10) % 10 == 1)
+				return "th";
+
+			switch (Math.Abs(number) % 10)
+			{
+				case 1:
+					return "st";
+				case 2:
+					return "nd";
+				case 3:
+					return "rd";
+				default:
+					return "th";
+			}
+		}
+	}
+}
